fc6265fefeeb7763744964c0398bb4b779abc78e
Krzysztof Czerniachowicz <k.czerniachowicz@gmail.com>
2025-10-14 08:44:52 +0200

    zebranie do calosci

---
+++ pea_proj_zad2/tsp_bxb_best.h
@@ -0,0 +1,195 @@
+#ifndef TSP_BEST
+#define TSP_BEST
+
+#include "graph.h"
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <chrono>
+#include <algorithm>
+#include "utils.h"
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <set>
+#include <climits>
+
+using namespace std;
+
+struct State_best {
+    vector<int> path;       
+    int cost;               
+    int level;              // Poziom w drzewie przeszukiwania
+    int index;
+};
+
+
+struct greater_cust {
+    bool operator()(const State_best& a, const State_best& b){
+        return a.cost > b.cost;
+    }
+};
+
+class Tsp_bxb_best {
+    Graph *_g;
+    int gSize;
+    vector<vector<int>> graph;
+    int bestRes = INT_MAX;
+    std::vector<int> best_cycle;
+
+    bool debug = true;
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> diff;
+    std::chrono::minutes maxtime = std::chrono::minutes(1);
+    bool overrun = false;
+
+    public:
+    Tsp_bxb_best(Graph& g, bool DEBUG, int MAX_TIME);
+    int solve();
+    vector<int> nearestNeighbourTSP();
+    void bfs_cycle(int i);
+    int getBestRes();
+};
+
+Tsp_bxb_best::Tsp_bxb_best(Graph& g, bool DEBUG, int MAX_TIME){
+    this->_g = &g;
+    debug = DEBUG;
+    maxtime = std::chrono::minutes(MAX_TIME);
+    gSize = g.getSize();
+    graph = g.getMatrix();
+}
+
+inline vector<int> Tsp_bxb_best::nearestNeighbourTSP()
+{
+    vector<int> path;
+    vector<bool> visited(gSize, false);
+    int currentCity = 0;
+    path.push_back(currentCity);
+    visited[currentCity] = true;
+    int totalCost = 0;
+
+    for (int i = 1; i < gSize; ++i) {
+        int nearest = -1;
+        int minDistance = INT_MAX;
+
+        for (int j = 0; j < gSize; ++j) {
+            if (!visited[j] && graph[currentCity][j] >= 0 && graph[currentCity][j] < minDistance) {
+                minDistance = graph[currentCity][j];
+                nearest = j;
+            }
+        }
+
+        if (nearest != -1) {
+            path.push_back(nearest);
+            visited[nearest] = true;
+            totalCost += minDistance;
+            currentCity = nearest;
+        }
+    }
+    totalCost += graph[currentCity][path[0]];
+    bestRes = totalCost;
+    return path;
+}
+
+inline void Tsp_bxb_best::bfs_cycle(int si)
+{
+    State_best root = {{si}, 0, 0, si};
+    priority_queue<State_best, vector<State_best>, greater_cust> q;
+    // queue<State> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        //sprawdzenie czy nie przekroczony maksymalny czas
+        end = std::chrono::system_clock::now();
+        diff = end - start;
+        auto tm = std::chrono::duration_cast<std::chrono::minutes>(diff);
+        if(tm >= maxtime) {
+            overrun = true;
+            return;
+        }
+
+        State_best current = q.top();
+        q.pop();
+
+        //czy obecny wierzcholek jest koncowy
+        if (current.level == gSize - 1) {
+            // current.path.push_back(i);
+            // czy wierzcholek koncowy sasiaduje z poczatkowym
+            int cost = graph[current.index][si];
+            if(cost > 0) {
+                current.cost += cost;
+                if (current.cost < bestRes) {
+                    bestRes = current.cost;
+                    best_cycle = current.path;
+                }
+            }
+        } else {
+            // dodaj sasiadow do kolejki
+            for (int i = 0; i < gSize; ++i) {
+                int cost = graph[current.index][i];
+                if(cost > 0 && find(current.path.begin(),current.path.end(),i) == current.path.end()) {
+                    State_best next;
+                    next.path = current.path;
+                    next.path.push_back(i);
+                    next.level = current.level + 1;
+                    next.cost = current.cost + cost;
+                    next.index = i;
+
+                    // jesli laczny koszt mniejszy niz obecnie najlepszy to dodaj do kolejki
+                    if (next.cost < bestRes) {
+                        q.push(next);
+                    }
+                }
+            }
+        }
+    }
+}
+
+inline int Tsp_bxb_best::getBestRes()
+{
+    return bestRes;
+}
+
+inline int Tsp_bxb_best::solve() 
+{   
+    int bestKnow = _g->getBestKnow();
+    int iterCount = 0;
+
+    vector<int> best_cycle = nearestNeighbourTSP();
+
+    start = std::chrono::system_clock::now();
+    
+
+    for(int i = 0; i < gSize; i ++) {
+        // zakoncz jesli odnaleziono optymalne rozwiazanie
+        if(bestRes == bestKnow) {
+            break;
+        }
+
+        bfs_cycle(i);
+        
+        //zakoncz szukanie rozwiazan jesli przekroczono czas
+        if(overrun) break;
+
+        iterCount++;
+
+        //print debug
+        if(debug && (iterCount % 1 == 0)) {
+            std::cout << iterCount << "/" << gSize << " best yet: " << bestRes << endl;
+        }
+    }
+
+    end = std::chrono::system_clock::now();
+    diff = end - start;
+    auto tms = std::chrono::duration_cast<std::chrono::milliseconds>(diff);
+
+    cout << "BxB BEST\nNajlepszy znaleziony cykl:";
+    for(int v : best_cycle) cout << v << " ";
+    cout << " Koszt: " << bestRes << endl;
+
+    return tms.count();
+}
+
+#endif
---
+++ pea_proj_zad2/tsp_bxb_dfs.h
@@ -0,0 +1,167 @@
+#ifndef TSP_DFS
+#define TSP_DFS
+
+#include "graph.h"
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <chrono>
+#include "utils.h"
+
+class Tsp_bxb_dfs {
+    Graph *_g;
+    int gSize;
+    int *visited;
+    int visited_count = 0;
+    bool found = false;
+    int si;
+    int bestRes = INT_MAX;
+    std::list<int> res_cycle;
+    std::list<int> best_cycle;
+
+    std::vector<Node> nodes;
+
+    bool debug = true;
+    int cycle_res_sum;
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> diff;
+    std::chrono::minutes maxtime = std::chrono::minutes(1);
+    bool overrun = false;
+
+    public:
+    Tsp_bxb_dfs(Graph& g, bool debug, int MAX_TIME);
+    int solve();
+    void hamil_cycle_step(int vi);
+    int getBestRes();
+};
+
+Tsp_bxb_dfs::Tsp_bxb_dfs(Graph& g, bool DEBUG, int MAX_TIME) {
+    this->_g = &g;
+    debug = DEBUG;
+    maxtime = std::chrono::minutes(MAX_TIME);
+    gSize = g.getSize();
+    nodes = g.getNodes();
+}
+
+inline int Tsp_bxb_dfs::solve() {
+    int bestKnow = _g->getBestKnow();
+    int iterCount = 0;
+    start = std::chrono::system_clock::now();
+    
+
+    for(int i = 0; i < gSize; i ++) {
+        si = i;
+        visited_count++;
+
+        // zakoncz jesli odnaleziono optymalne rozwiazanie
+        if(bestRes == bestKnow) {
+            break;
+        }
+
+        visited = new int[gSize];
+        for(int i = 0; i < gSize; i++) {
+            visited[i] = 0;
+        }
+
+        hamil_cycle_step(si);
+        
+        //zakoncz szukanie rozwiazan jesli przekroczono czas
+        if(overrun) break;
+
+        iterCount++;
+
+        //print debug
+        if(debug && (iterCount % 1 == 0)) {
+            std::cout << iterCount << "/" << gSize << " best yet: " << bestRes << endl;
+        }
+    }
+
+    end = std::chrono::system_clock::now();
+    diff = end - start;
+    auto tms = std::chrono::duration_cast<std::chrono::milliseconds>(diff);
+
+    cout << "BxB DFS\nNajlepszy znaleziony cykl:";
+    for(int v : best_cycle) cout << v << " ";
+    cout << " Koszt: " << bestRes << endl;
+
+    return tms.count();
+}
+
+inline void Tsp_bxb_dfs::hamil_cycle_step(int vi)
+{   
+    //sprawdzenie czy nie przekroczony maksymalny czas
+    end = std::chrono::system_clock::now();
+    diff = end - start;
+    auto tm = std::chrono::duration_cast<std::chrono::minutes>(diff);
+    // jesli tak to wyjdz z rekurencji
+    if(tm >= maxtime) {
+        overrun = true;
+        return;
+    }
+
+    Node &currentNode = nodes[vi];
+    const std::vector<vector<int>> neightmatrix = _g->matrix;
+    std::vector<int> v_neighs;
+    std::vector<int> v_row = neightmatrix.at(vi);
+
+    int ni = si;
+
+    //vi neight from matrix
+    for(int i = 0; i < gSize; i++) {
+        int w = v_row.at(i);
+        if(w > 0){
+            v_neighs.push_back(i);
+        } 
+    }
+
+    for(int ni : v_neighs) {
+        if(visited[ni] == 0 && ni != si) {
+            Node &nextNode = nodes[ni];
+            nextNode.weight = currentNode.weight + v_row[ni];
+            nextNode.parentIndex = vi;
+
+            //jeśli koszt dojscia do sąsiada większy niż dotychczasowy najlepszy koszt cyklu to pomin
+            if(nextNode.weight >= bestRes) continue;
+
+            visited[ni] = 1;
+            visited_count++;
+            hamil_cycle_step(ni);
+            if(overrun) return;
+        }
+        else if (visited_count == gSize && ni == si) {
+            //hamil cycle found
+            int cycle_cost = currentNode.weight + v_row[ni];
+            if (cycle_cost < bestRes) {
+                bestRes = cycle_cost;
+                best_cycle.clear();
+                
+                int btnode = vi;
+                int pi = nodes[vi].parentIndex;
+                while(pi != -1) {
+                    best_cycle.push_front(btnode);
+                    btnode = pi;
+                    pi = nodes[btnode].parentIndex;
+                }
+                best_cycle.push_front(btnode);
+
+                if(debug) {
+                    for( int v : best_cycle) cout << v << " ";
+                    cout << "koszt: " << cycle_cost << "\n";
+                }
+            }
+        }
+    }
+    
+    visited_count--;
+    visited[vi] = 0;
+    currentNode.weight = 0;
+    currentNode.parentIndex = -1;
+}
+
+inline int Tsp_bxb_dfs::getBestRes()
+{
+    return bestRes;
+}
+
+#endif
